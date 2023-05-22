428d2aaa3bd972d3dfb86daaefa40d18546b6aec
Jackline-ke <antoninahodhiambo@gmail.com>
2023-05-22 18:40:27 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+#Book  class
 
+class Book
+    attr_reader :title
+    attr_accessor :page_count, :genre, :author
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+end
 
